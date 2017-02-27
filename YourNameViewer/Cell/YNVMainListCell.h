0fcc985a594a8b9cc34d4e555da2b5e1f860f0bd
Jaewon Noh <opqw987@gmail.com>
2017-02-27 17:44:43 +0900

    Add 'YNVMainListCell' and setup

---
+++ YourNameViewer/Cell/YNVMainListCell.h
@@ -0,0 +1,17 @@
+//
+//  YNVMainListCell.h
+//  YourNameViewer
+//
+//  Created by 노재원 on 2017. 2. 27..
+//  Copyright © 2017년 heroin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YNVMainListCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+@property (strong, nonatomic) IBOutlet UILabel *userNameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *postDateLabel;
+
+@end
---
+++ YourNameViewer/Cell/YNVMainListCell.m
@@ -0,0 +1,24 @@
+//
+//  YNVMainListCell.m
+//  YourNameViewer
+//
+//  Created by 노재원 on 2017. 2. 27..
+//  Copyright © 2017년 heroin. All rights reserved.
+//
+
+#import "YNVMainListCell.h"
+
+@implementation YNVMainListCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
