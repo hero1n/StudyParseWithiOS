27a5150be380356bea3bfed1b0792515afa61933
Jaewon Noh <opqw987@gmail.com>
2017-02-27 11:51:41 +0900

    Add Prefix header and constants file, fix delegate from

---
+++ YourNameViewer/YNVConstants.h
@@ -0,0 +1,25 @@
+//
+//  YNVConstants.h
+//  YourNameViewer
+//
+//  Created by 노재원 on 2017. 2. 27..
+//  Copyright © 2017년 heroin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef YNVConstants_h
+#define YNVConstants_h
+#ifdef DEBUG
+#define NSLog( s, ... ) NSLog( @"%d Line, %s :: %@", __LINE__, __PRETTY_FUNCTION__, [NSString stringWithFormat:(s), ##__VA_ARGS__] )
+#else
+#define NSLog( s, ... )
+#endif
+
+#define GALLERY_MAIN_LIST_URL @"http://gall.dcinside.com/board/lists?id=yourname"
+
+#endif /* YNVConstant_h */
+
+@interface YNVConstants : NSObject
+
+@end
