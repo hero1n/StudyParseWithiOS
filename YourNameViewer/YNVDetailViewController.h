fb18cd2a841b8887aeddf40d7a309ba11458c0fa
Jaewon Noh <opqw987@gmail.com>
2017-02-28 11:35:16 +0900

    Add detail view controller

---
+++ YourNameViewer/YNVDetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  YNVDetailViewController.h
+//  YourNameViewer
+//
+//  Created by 노재원 on 2017. 2. 27..
+//  Copyright © 2017년 heroin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YNVDetailViewController : UIViewController
+
+@property (strong, nonatomic, nonnull) NSString *postLinkString;
+
+@end
---
+++ YourNameViewer/YNVDetailViewController.m
@@ -0,0 +1,22 @@
+//
+//  YNVDetailViewController.m
+//  YourNameViewer
+//
+//  Created by 노재원 on 2017. 2. 27..
+//  Copyright © 2017년 heroin. All rights reserved.
+//
+
+#import "YNVDetailViewController.h"
+
+@interface YNVDetailViewController ()
+
+@end
+
+@implementation YNVDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
