a73ebacad91528f7d8b631acab063979b56b6f20
Jaewon Noh <opqw987@gmail.com>
2017-03-07 19:03:03 +0900

    Add comment (not working) and add more more more more post parse filter
    I really don't know this commit

--- YourNameViewer/YNVDetailViewController.m
+++ YourNameViewer/YNVDetailViewController.m
@@ -10,6 +10,12 @@
 
 // Libraries
 #import "TFHpple.h"
+#import "IGHTMLQuery.h"
+
+// Cell
+#import "YNVDetailCommentCell.h"
+
+static NSString *const cellIdentifier = @"detailCommentCell";
 
 @interface YNVImageTextAttachment : NSTextAttachment
 
@@ -21,9 +27,23 @@
 
 @end
 
-@interface YNVDetailViewController ()
+@interface YNVCommentData : NSObject
+
+@property (strong, nonatomic) NSString *content;
+@property (strong, nonatomic) NSString *commenter;
+@property (strong, nonatomic) NSString *dateString;
+
+@end
+
+@implementation YNVCommentData
+
+@end
+
+@interface YNVDetailViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @property (strong, nonatomic) UIRefreshControl *refreshControl;
+
+@property (strong, nonatomic, nonnull) NSMutableArray<YNVCommentData *> *commentArray;
 
 @end
 
@@ -32,21 +52,68 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.commentArray = [NSMutableArray array];
+    
     self.refreshControl = [[UIRefreshControl alloc] init];
     [self.refreshControl addTarget:self
                             action:@selector(refresh)
                   forControlEvents:UIControlEventValueChanged];
-    [self.textView addSubview:self.refreshControl];
-    self.textView.scrollEnabled = YES;
+    [self.scrollView addSubview:self.refreshControl];
     
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+    self.tableView.estimatedRowHeight = 100.0f;
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.hidden = YES;
+    
+    [self getPostWithIGHTML];
     [self getPost];
 }
 
 - (void)refresh {
     self.textView.text = @"";
     
+    [self getPostWithIGHTML];
     [self getPost];
     [self.refreshControl endRefreshing];
+}
+
+- (void)getPostWithIGHTML {
+    [self.view makeToastActivity:CSToastPositionCenter];
+    NSMutableAttributedString *contentString = [[NSMutableAttributedString alloc]
+                                                initWithString:[self.listData.title stringByAppendingString:@"\n"]
+                                                attributes:@{NSFontAttributeName : [UIFont boldSystemFontOfSize:15.0f]}
+                                                ];
+    
+    [contentString appendAttributedString:[[NSMutableAttributedString alloc]
+                                           initWithString:[self.listData.userName stringByAppendingString:@"    "]
+                                           attributes:@{NSFontAttributeName : [UIFont systemFontOfSize:14.0f],
+                                                        NSForegroundColorAttributeName : [UIColor lightGrayColor]}
+                                           ]];
+    
+    [contentString appendAttributedString:[[NSMutableAttributedString alloc]
+                                           initWithString:self.listData.postDateString
+                                           attributes:@{NSFontAttributeName : [UIFont systemFontOfSize:14.0f],
+                                                        NSForegroundColorAttributeName : [UIColor lightGrayColor]}
+                                           ]];
+    
+    [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:@"\n\n"]];
+    
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0), ^{
+        NSString *postURLString = [GALLERY_END_POINT stringByAppendingString:self.listData.postLink];
+        IGHTMLDocument *document = [[IGHTMLDocument alloc] initWithHTMLString:postURLString error:nil];
+        NSString *XPathQuery = @"//*[@id='dgn_content_de']";
+        
+        IGXMLNodeSet *contents = [document queryWithXPath:XPathQuery];
+        [contents enumerateNodesUsingBlock:^(IGXMLNode* content, NSUInteger idx, BOOL *stop){
+            NSLog(@"fsd %@", content.xml);
+        }];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.view hideToastActivity];
+            self.textView.attributedText = contentString;
+        });
+    });
 }
 
 - (void)getPost {
@@ -72,7 +139,6 @@
     
     dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0), ^{
         NSString *XPathQuery = @"//*[@id='dgn_content_de']/div[2]/div[1]/div";
-        //*[@id="dgn_content_de"]/div[2]/div[1]/div/table/tbody/tr
         NSString *postURLString = [GALLERY_END_POINT stringByAppendingString:self.listData.postLink];
         NSURL *postURL = [NSURL URLWithString:postURLString];
         NSData *postHTMLData = [NSData dataWithContentsOfURL:postURL];
@@ -103,7 +169,45 @@
         for (TFHppleElement *element in tdElement.children) {
             NSLog(@"<<<<<< %@ <> %@ <> %@ <> %@ >>>>>>>", element.text, element.tagName, element.attributes, element.children);
             
-            if ([element isTextNode]) {
+            if ([element.tagName isEqualToString:@"p"]) {
+                if ([element.firstChild.tagName isEqualToString:@"img"]) {
+                    NSString *srcString = element.firstChild.attributes[@"src"];
+                    
+                    if (srcString.length > 0) {
+                        [contentString appendAttributedString:[NSMutableAttributedString attributedStringWithAttachment:[self textAttachmentWithImageURL:srcString]]];
+                        [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:@" \n"]];
+                    }
+                }
+                
+                if (element.text.length > 0) {
+                    [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:element.text]];
+                    [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:@" \n"]];
+                } else {
+                    [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:@" \n"]];
+                }
+            } else if ([element.tagName isEqualToString:@"br"]) {
+                [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:@" \n"]];
+            } else if ([element.tagName isEqualToString:@"text"]) {
+                [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:element.content]];
+            } else if ([element.tagName isEqualToString:@"a"]) {
+                NSString *srcString = element.firstChild.attributes[@"src"];
+                NSString *childSrcString = [element firstChildWithTagName:@"img"].attributes[@"src"];
+                
+                if (srcString.length > 0) {
+                    [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:srcString]];
+                } else if (childSrcString.length > 0) {
+                    [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:childSrcString]];
+                } else {
+                    [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:element.attributes[@"href"]]];
+                }
+            } else if ([element.tagName isEqualToString:@"img"]) {
+                NSString *srcString = element.attributes[@"src"];
+                
+                if (srcString.length > 0) {
+                    [contentString appendAttributedString:[NSMutableAttributedString attributedStringWithAttachment:[self textAttachmentWithImageURL:srcString]]];
+                    [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:@" \n"]];
+                }
+            } else if ([element.tagName isEqualToString:@"span"]) {
                 if (element.text.length > 0) {
                     [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:element.text]];
                 } else if (element.content.length > 0) {
@@ -118,30 +222,38 @@
                         [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:childElement.content]];
                     }
                 }
-            } else if ([element.tagName isEqualToString:@"br"]) {
-                [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:@" \n"]];
-            } else if ([element.tagName isEqualToString:@"text"]) {
-                [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:element.content]];
+            } else if (element.attributes[@"app_paragraph"] != nil) {
+                if ([((NSString *)element.attributes[@"app_paragraph"]) containsString:@"Dc_App_text"]) {
+                    if (element.text.length > 0) {
+                        [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:element.text]];
+                    }
+                    if (element.content.length > 0) {
+                        [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:element.content]];
+                    }
+                }
+                if ([((NSString *)element.attributes[@"app_paragraph"]) containsString:@"Dc_App_Img"]) {
+                    NSString *srcString = [element firstChildWithTagName:@"img"].attributes[@"src"];
+                    
+                    if (srcString.length > 0) {
+                        [contentString appendAttributedString:[NSMutableAttributedString attributedStringWithAttachment:[self textAttachmentWithImageURL:srcString]]];
+                        [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:@" \n"]];
+                    }
+                }
             } else if ([element.tagName isEqualToString:@"div"] ||
                        [((TFHppleElement *)element.children.firstObject).tagName isEqualToString:@"img"] ||
                        [element firstChildWithTagName:@"img"]) {
-                NSString *srcString = ((TFHppleElement *)element.children.firstObject).attributes[@"src"];
+                NSString *srcString = [element firstChildWithTagName:@"img"].attributes[@"src"];
                 
                 if (srcString.length > 0) {
                     [contentString appendAttributedString:[NSMutableAttributedString attributedStringWithAttachment:[self textAttachmentWithImageURL:srcString]]];
-                }
-            } else if ([element.tagName isEqualToString:@"img"]) {
-                NSString *srcString = element.attributes[@"src"];
-                
-                if (srcString.length > 0) {
-                    [contentString appendAttributedString:[NSMutableAttributedString attributedStringWithAttachment:[self textAttachmentWithImageURL:srcString]]];
+                    [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:@" \n"]];
                 }
             } else if (element.hasChildren) {
                 for (TFHppleElement *child in element.children) {
                     if (child.content != nil) {
                         [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:child.content]];
                     }
-                    if ([child.children count]!= 0) {
+                    if ([child.children count] != 0) {
                         for (TFHppleElement *grandchild in child.children) {
                             if (grandchild.content != nil) {
                                 [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:grandchild.content]];
@@ -162,22 +274,53 @@
                         }
                     }
                 }
-            } else if ([element.tagName isEqualToString:@"a"]) {
-                [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:element.attributes[@"href"]]];
-            } else if ([element.tagName isEqualToString:@"span"]) {
-                if (element.text.length > 0) {
-                    [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:element.text]];
-                } else if (element.content.length > 0) {
-                    [contentString appendAttributedString:[[NSMutableAttributedString alloc] initWithString:element.content]];
-                }
             }
         }
         
         NSLog(@"final text: %@", contentString);
         
+        [contentString appendAttributedString:[[NSAttributedString alloc] initWithString:@" "]];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.textView sizeToFit];
+            self.textView.attributedText = contentString;
+            [self.textView sizeToFit];
+            [self.textView layoutIfNeeded];
+            [self.textView sizeToFit];
+            
+            [self.view hideToastActivity];
+//            [self getComments];
+        });
+    });
+}
+
+- (void)getComments {
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0), ^{
+        NSString *XPathQuery = @"/html/head";
+        NSString *postURLString = [GALLERY_END_POINT stringByAppendingString:
+                                   [self.listData.postLink
+                                    stringByReplacingCharactersInRange:[self.listData.postLink rangeOfString:@"view"]
+                                    withString:@"comment_view"]
+                                   ];
+        NSURL *postURL = [NSURL URLWithString:postURLString];
+        NSData *postHTMLData = [NSData dataWithContentsOfURL:postURL];
+        
+        TFHpple *postParser = [TFHpple hppleWithData:postHTMLData isXML:NO];
+        NSArray<TFHppleElement *> *parsedArray = [postParser searchWithXPathQuery:XPathQuery];
+        
+        NSLog(@"%@", [postParser peekAtSearchWithXPathQuery:XPathQuery]);
+        
+        for (TFHppleElement *parsedElement in parsedArray) {
+//            NSLog(@"<<<<<< %@ <> %@ <> %@ <> %@ >>>>>>>", parsedElement.text, parsedElement.tagName, parsedElement.attributes, parsedElement.children);
+            if (parsedElement.content.length > 0) {
+                NSLog(@"%@", parsedElement.content);
+            }
+        }
+        
         dispatch_async(dispatch_get_main_queue(), ^{
             [self.view hideToastActivity];
-            self.textView.attributedText = contentString;
+            
+            [self.tableView reloadData];
         });
     });
 }
@@ -188,7 +331,12 @@
     UIImage *image = [UIImage imageWithData:imageData];
     
     YNVImageTextAttachment *textAttachment = [[YNVImageTextAttachment alloc] init];
-    textAttachment.image = [self imageWithImage:image scaledToWidth:self.view.frame.size.width];
+    textAttachment.image = image;
+    
+    if (image.size.width > self.view.frame.size.width) {
+        textAttachment.image = [self imageWithImage:image scaledToWidth:self.view.frame.size.width];
+    }
+    
     textAttachment.imageURLString = imageSrc;
     
     return textAttachment;
@@ -208,5 +356,27 @@
     
     return newImage;
 }
+
+#pragma mark - Table view datasource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.commentArray.count;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return @"댓글";
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    YNVDetailCommentCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
 
 @end
